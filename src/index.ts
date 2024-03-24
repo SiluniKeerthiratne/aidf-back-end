73a4b9077b2a69640ffa98d3a97915a87b4bb2c9
Siluni Keerthiratne <siluni@keerthiratne.com>
2024-03-24 12:11:48 +0530

    dynamic port fixed

--- src/index.ts
+++ src/index.ts
@@ -14,4 +14,5 @@ connectDB();
 app.use("/jobs", jobsRouter);
 app.use("/jobApplications", jobApplicationsRouter);
 
-app.listen(8000, () => console.log("Server is listening on port 8000."));
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
